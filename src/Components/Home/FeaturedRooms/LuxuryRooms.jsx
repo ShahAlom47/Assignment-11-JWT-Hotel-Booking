7b56830e809513d2c562356e9d079ed1393caffc
sah alom <sahalom4729@gmail.com>
2024-05-10 03:13:12 +0400

    added rooms section

--- src/Components/Home/FeaturedRooms/LuxuryRooms.jsx
+++ src/Components/Home/FeaturedRooms/LuxuryRooms.jsx
@@ -1,12 +1,63 @@
+import { useEffect, useState } from "react";
+import useAxiosSecure from "../../CustomHockes/useAxios";
 
 
 const LuxuryRooms = () => {
+    const [hoveredIndex, setHoveredIndex] = useState(null);
+    const [rooms, setRooms] = useState([]);
+
+    const axiosSecure = useAxiosSecure()
+
+    useEffect(() => {
+        axiosSecure.get(('/rooms'))
+            .then(data => setRooms(data?.data))
+    }, [axiosSecure])
+
+    console.log(rooms);
+
+    function handleMouseOver(index) {
+        setHoveredIndex(index);
+    }
+
+    function handleMouseOut() {
+        setHoveredIndex(null);
+    }
     return (
-        <div>
-            <div className=" my-11">
-                <h1 className="text-center text-2xl " >Luxury Rooms & Suites</h1>
+        <div className="bg-[#f8f5f0] py-20 my-10">
+            <div className=" mb-16">
+                <h1 className="text-center text-3xl " >Luxury Rooms & Suites</h1>
             </div>
-            
+            <div className="max-w grid gap-5 md:grid-cols-2 lg:grid-cols-2">
+                {
+                    rooms.map((data, index) => <div key={data._id} className="  rounded-sm shadow-2xl shadow-gray-500">
+
+                        <div
+                            onMouseOver={() => handleMouseOver(index)}
+                            onMouseOut={handleMouseOut}
+                            className="  w-full  relative  image-full rounded-sm h-60 overflow-hidden  duration-300 btn bg-none p-0">
+                            <figure className="w-full h-full"><img
+                                style={{ transition: 'transform 0.9s ease', transform: hoveredIndex === index ? 'scale(1.20)' : 'scale(1)' }}
+
+                                className="rounded-md w-full h-full" src={data.image} alt="Shoes" /></figure>
+                            <h2 className="card-title mb-8 text-gray-200 font-semibold absolute top-3/4  -left-0 bg-[#000000c3] px-9 py-2 rounded-r-full"
+                                style={{ transition: 'transform 0.4s ease', transform: hoveredIndex === index ? 'translateY(-108%)' : 'translateY(-0%)' }}
+                            >{data.description}</h2>
+                            <div className="p-4 flex justify-between items-start w-full relative bg-[#00000080]  text-gray-800 "
+                                style={{ transition: 'transform 0.9s ease', transform: hoveredIndex === index ? 'translateY(-70%)' : 'translateY(-0%)' }}>
+                                 
+                                <h2 className="card-title mb-8 font-semibold text-gray-100 ">$ {data.price_per_night} /NIGHT </h2>
+
+                                <button className="btn absolute bg-gray-700 pr-16 border-none rounded-l-full -right-2 bottom-10  text-gray-50 p-3">Book Now</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    )
+
+                }
+
+            </div>
+
         </div>
     );
 };
--- src/Components/Root.jsx
+++ src/Components/Root.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./SharedComponets/Navbar";
+import Footer from "./SharedComponets/Footer";
 
 
 const Root = () => {
@@ -7,6 +8,8 @@ const Root = () => {
         <div>
             <Navbar></Navbar>
            <Outlet></Outlet>
+           <Footer></Footer>
+           
             
         </div>
     );
