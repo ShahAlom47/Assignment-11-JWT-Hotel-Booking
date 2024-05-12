f0b68ae7b4e2f52edc4dc50bbe998b2a8ae1d333
sah alom <sahalom4729@gmail.com>
2024-05-12 19:51:59 +0400

    added sort functionality in review setcion

--- src/Components/Home/UserReviewPage/UserReview.jsx
+++ src/Components/Home/UserReviewPage/UserReview.jsx
@@ -20,7 +20,7 @@ const UserReview = () => {
 
 
 
- 
+ console.log(sortValu);
 
 
 
@@ -69,18 +69,18 @@ const UserReview = () => {
         }}>
             <div className="max-w flex justify-end items-center">
                <div className="Lg:w-8/12 md:w-8/12 w-11/12 flex justify-center items-start flex-col ">
-                <div className="text-start px-5 lg:pr-7 flex justify-around items-center w-full">
+                <div className="text-start px-5 lg:pr-7 flex justify-between items-center w-full">
 
                     <h1 className="text-white text-3xl mb-4">What Client`s Say?</h1>
                     <div className="dropdown dropdown-bottom dropdown-end">
                         <div
                             tabIndex={0}
                             role="button"
-                            className="btn btn-sm mx-1 hover:text-gray-900 text-gray-900  rounded-sm border-none border-b-2 bg-slate-50  "
+                            className="btn btn-sm mx-1 hover:text-gray-900 text-gray-900  rounded-sm border-none border-b-2 bg-[#ffffff76]  "
                         >Sort By Date</div>
                         <ul tabIndex={0} className=" text-gray-50 dropdown-content z-[1] menu px-2 shadow rounded-sm w-52">
-                            <li><a onClick={() => setSortValue('recently')} className={`border-b-2  ${sortValu === 'ase' ? 'bg-slate-600' : 'bg-slate-800'}`}>Recently</a> </li>
-                            <li><a onClick={() => setSortValue('random')} className={`border-b-2  ${sortValu === 'dise' ? 'bg-slate-600' : 'bg-slate-800'}`}>Random</a></li>
+                            <li><a onClick={() => setSortValue('recently')} className={`border-b-2  ${sortValu === 'recently' ? 'bg-slate-600' : 'bg-slate-800'}`}>Recently</a> </li>
+                            <li><a onClick={() => setSortValue('random')} className={`border-b-2  ${sortValu === 'random' ? 'bg-slate-600' : 'bg-slate-800'}`}>Random</a></li>
                         </ul>
                     </div>
                 </div>
@@ -91,14 +91,20 @@ const UserReview = () => {
                                 <Slider {...settings} className="w-full items-center flex justify-end rounded-lg">
                                     {
                                         userReview.map(data => <div key={data._id}>
-                                            <div className="card rounded-md  bg-[#f8efefdd] shadow-xl m-4 overflow-hidden h-[250px]">
+                                            <div className="card rounded-md  bg-[#f8efefdd] shadow-lg shadow-gray-500 m-4 overflow-hidden h-[250px]">
                                                 <div className="  p-3">
-                                                <div className="flex gap-5">  
-                                                    <h1 className="text-lg text-black  ">{data.userName}</h1>
                                                     
-                                                    <p>{data.rating && Array(parseInt(data.rating)).fill().map((_, index) => <span className="text-xs" key={index}>&#9733;</span>)}</p>
+                                                <div className=" flex items-center gap-2 border-b-2 border-black pb-2">  
+                                                    <div className="h-10 w-10 border rounded-full border-black">
+                                                        <img className="h-full w-full rounded-full" src={data.photoURL} alt="" />
+                                                    </div>
+                                                  <div>
+                                                  <h1 className="text-lg text-black  ">{data.userName}</h1>
+                                                   <p className=" text-xs ">{data.time}</p>
                                                   </div>
-                                                   <p className=" text-xs border-b">{data.time}</p>
+                                                    
+                                                  </div>
+                                                    <p>{data.rating && Array(parseInt(data.rating)).fill().map((_, index) => <span className="text-xs text-red-700 pl-2" key={index}>&#9733;</span>)}</p>
                                                    <p className="py-3 text-sm">{data.comment}</p>
                                                     
                                                 </div>
