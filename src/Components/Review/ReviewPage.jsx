dd9b5255760bb8b6ee4859e9dc35112bdff1697a
sah alom <sahalom4729@gmail.com>
2024-05-12 09:04:48 +0400

    added review validation

--- src/Components/Review/ReviewPage.jsx
+++ src/Components/Review/ReviewPage.jsx
@@ -7,7 +7,9 @@ import useAxiosSecure from '../CustomHockes/useAxios';
 
 const ReviewPage = ({ roomId }) => {
     const [reviews, setReviewData] = useState([]);
-    const [r, setRData] = useState('');
+    const [myBooking, setMyBooking] = useState([]);
+    const [isBooked, setBooked] = useState(false)
+    const [rData, setRdata] = useState({})
     const axiosSecure = useAxiosSecure()
     const { user } = useContext(AuthContext);
     const dates = new Date()
@@ -26,8 +28,25 @@ const ReviewPage = ({ roomId }) => {
 
         axiosSecure(`reviews/${roomId}`)
             .then((data) => { setReviewData(data?.data) })
-    }, [axiosSecure, roomId,  user])
+    }, [axiosSecure, roomId, user,rData])
 
+
+    useEffect(() => {
+        axiosSecure.get(`/booking?email=${user?.email}`)
+            .then(data => setMyBooking(data.data))
+            .catch(err => console.log(err))
+    }, [axiosSecure, user,])
+
+    console.log(myBooking);
+
+
+    // Found valid users for review
+    useEffect(() => {
+        const IsBook = myBooking.find(data => data.roomId === roomId)
+        if (IsBook) {
+            setBooked(true)
+        }
+    }, [myBooking, roomId])
 
 
 
@@ -39,11 +58,11 @@ const ReviewPage = ({ roomId }) => {
         const rating = form.rating.value;
         const reviewData = { comment, rating, time, userName, roomId, photoURL }
 
-     
-  
-
         axiosSecure.post('/review', { reviewData })
-            .then((res) => form.reset())
+            .then((res) => {
+                setRdata(reviewData)
+                form.reset()
+            })
             .catch(err => console.log(err))
     }
 
@@ -68,8 +87,8 @@ const ReviewPage = ({ roomId }) => {
                                     <div className=" ">
                                         <div className='flex gap-7'>
                                             <p className='text-lg font-semibold'>{data?.userName}</p>
-                                            
-                                            <p>{ data.rating&&Array(parseInt(data.rating)).fill().map((_,index)=>  <span key={index}>&#9733;</span>)}</p>
+
+                                            <p>{data.rating && Array(parseInt(data.rating)).fill().map((_, index) => <span key={index}>&#9733;</span>)}</p>
                                         </div>
                                         <div>
                                             <p className='text-xs'>{data?.time}</p>
@@ -91,14 +110,17 @@ const ReviewPage = ({ roomId }) => {
             <hr />
 
 
-            <form onSubmit={reviewHandel} className='flex items-end my-10 w-full'>
+            <form onSubmit={reviewHandel} className={`flex items-end mt-10 w-full `}>
                 <div className='border-2 flex flex-col w-full' >
                     <input type="number" name="rating" max={5} min={1} placeholder='Rating' id="" required />
                     <textarea className='border-t-2 w-full' name="comment" id="" cols="30" rows='4' placeholder='Write your own opinion here' required></textarea>
                 </div>
-                <input type="submit" className='btn-pry' value="Review" />
+                <input type="submit" className={`btn-pry  `} value="Review" disabled={isBooked ? false : true} />
 
             </form>
+            {
+                !isBooked && <p className='text-red-500'>!!! Rom booking will be required for review</p>
+            }
         </div>
     );
 };
